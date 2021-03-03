2f39057bd2e8082366045668182797d33e6349e3
dnuthakki3 <43687763+dnuthakki3@users.noreply.github.com>
2021-03-03 09:43:45 -0500

    Update loginserv.php

--- serv/loginserv.php
+++ serv/loginserv.php
@@ -15,7 +15,7 @@ if(isset($_POST['login'])){
    //Establishing Connection with server by passing server_name, user_id and pass as a patameter
    $conn = mysqli_connect("localhost", "[username redacted]", "[password redacted]");
    //Selecting Database
-   $db = mysqli_select_db($conn, "loginsystemfbla");
+   $db = mysqli_select_db($conn, "[database name redacted]");
    //sql query to fetch information of registerd user and finds user match.
    $fetch_query = mysqli_query($conn, "SELECT * FROM Users WHERE pass='$pass_hash' AND user='$uname_input'");
    //sees if the query results in any rows being returned
