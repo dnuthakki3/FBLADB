d582a17912dd89fec4a932e62a73b2dd936b4733
dnuthakki3 <43687763+dnuthakki3@users.noreply.github.com>
2021-03-03 09:51:40 -0500

    Update tableserv.php

--- serv/tableserv.php
+++ serv/tableserv.php
@@ -5,20 +5,26 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+    //Select statement used to pull data from the table in the database
     $sql = "SELECT sid, l_name, f_name, grade, c_event  FROM Members";
     $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
+    //Checks to see if there are any rows returned by the 
+    if ($result->num_rows > 0) {
         // output data of each row
-        while($row = $result->fetch_assoc()) {
-        echo "<tr>
-            <td>" . $row["sid"]. "</td>
-            <td>" . $row["l_name"] . "</td>
-            <td>" . $row["f_name"]. "</td>
-            <td>" . $row["grade"]. "</td>
-            <td>" . $row["c_event"]. "</td>
-        </tr>";
-}
-echo "</table>";
-} else { echo "0 results"; }
-$conn->close();
+         while($row = $result->fetch_assoc()) {
+            echo "<tr>
+                <td>" . $row["sid"]. "</td>
+                <td>" . $row["l_name"] . "</td>
+                <td>" . $row["f_name"]. "</td>
+                <td>" . $row["grade"]. "</td>
+                <td>" . $row["c_event"]. "</td>
+                  </tr>";
+                }
+              echo "</table>";
+            } 
+      else { 
+          echo "0 results"; 
+      }
+    //closes the connection at the end
+    $conn->close();
 ?>
